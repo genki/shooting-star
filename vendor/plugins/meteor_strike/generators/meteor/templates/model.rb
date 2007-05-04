a3bdaf93b5ba015262859589f3da48650f12bb6c
takiuchi <takiuchi@2674dce3-83ad-4ecd-a811-8fe676e38421>
2007-05-04 17:51:37 +0000

    Updated plugin.
    git-svn-id: http://shooting-star.rubyforge.org/svn/trunk@26 2674dce3-83ad-4ecd-a811-8fe676e38421

--- vendor/plugins/meteor_strike/generators/meteor/templates/model.rb
+++ vendor/plugins/meteor_strike/generators/meteor/templates/model.rb
@@ -1,9 +1,43 @@
 require 'drb/drb'
 
 class Meteor < ActiveRecord::Base
+  class Shooter
+    COUNTERS = [:count, :count_with]
+    LISTINGS = [:listeners, :listeners_with, :channels, :signatures]
+
+    def initialize(config)
+      uris = config['shooting_star']['shooter']
+      @shooters = [uris].flatten.map{|uri| DRbObject.new_with_uri(uri)}
+    end
+
+    COUNTERS.each do |m|
+      define_method(m){|*a| call(m, *a).inject(0){|r,c| r += c}}
+    end
+
+    LISTINGS.each do |m|
+      define_method(m){|*a| call(m, *a).inject([]){|r,c| r.concat c}}
+    end
+
+    def method_missing(method, *args, &block)
+      call(method, *args, &block).first
+    end
+
+  private
+    def call(method, *args, &block)
+      @shooters.inject([]) do |result, shooter|
+        begin
+          result << shooter.__send__(method, *args, &block)
+        rescue Exception => e
+          logger.error "#{e.message}\n#{e.backtrace.join("\n")}"
+        ensure
+          result
+        end
+      end
+    end
+  end
+
   def self.shooter
-    @@shooter ||= DRbObject.new_with_uri(
-      configurations[RAILS_ENV]['shooting_star']['shooter'])
+    @@shooter ||= ::Meteor::Shooter.new(configurations[RAILS_ENV])
   end
 
   def self.shoot(channel, javascript, tag = [])
